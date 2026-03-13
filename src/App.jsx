ff8015f5105a39859815cd15d65c2bd4e0bbbc6e
2400032275 <2400032275@kluniversity.in>
2026-03-14 00:00:10 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import React from "react";
+import Dashboard from "./components/Dashboard";
 import StudentManager from "./components/StudentManager";
 
 function App() {
-
-return (
-<div>
-<StudentManager/>
-</div>
-);
-
+  return (
+    <div>
+      <StudentManager />
+      <hr />
+      <Dashboard />
+    </div>
+  );
 }
 
 export default App;
